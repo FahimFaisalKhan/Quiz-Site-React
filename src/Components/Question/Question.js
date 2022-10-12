e11cb888aa00bc4f8bb3c2ceaf17a406b83a3fee
Fahim <KhanFahimFaisal@outlook.com>
2022-10-12 18:23:39 +0600

    added score count

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -4,27 +4,21 @@ import { parse } from "../Utilities/parser";
 import "./Question.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
-const Question = ({ questionData, serial }) => {
+const Question = ({ questionData, serial, verifyAnswer }) => {
   const [toast, setToast] = useState();
   const [showAnswer, setShowAnswer] = useState(false);
   const { options, question, id, correctAnswer } = questionData;
-  const verifyAnswer = (selectedAnswer) => {
-    if (selectedAnswer === correctAnswer) {
-      setToast({ text: "Good Job!! Correct Answer.", status: "success" });
-    } else {
-      setToast({ text: "Sorry!!  Wrong Answer", status: "error" });
-    }
-  };
+
   const parsedQuestion = parse(question).innerText;
   console.log(parsedQuestion);
   return (
     <div className=" bg-slate-400 mb-32 text-base-100 py-8 px-2 sm:p-12 rounded-lg ">
-      <div className="flex flex-col-reverse items-end sm:flex-row mb-8 justify-between">
+      <div className="flex flex-col-reverse  sm:flex-row mb-8 justify-between">
         <h1 className="text-xl font-medium ">
           Quiz : {serial} {parsedQuestion}
         </h1>
         <div
-          className="cursor-pointer"
+          className="cursor-pointer self-end sm:self-auto"
           onClick={() => setShowAnswer(!showAnswer)}
         >
           {showAnswer ? (
@@ -49,7 +43,9 @@ const Question = ({ questionData, serial }) => {
                 <Form.Label
                   className="p-8 bg-base-100 rounded-lg"
                   title={option}
-                  onClick={() => verifyAnswer(option)}
+                  onClick={() =>
+                    verifyAnswer(option, correctAnswer, setToast, id)
+                  }
                 >
                   <Radio
                     name="radio1"
@@ -71,7 +67,7 @@ const Question = ({ questionData, serial }) => {
         </div>
       </div>
       {toast && (
-        <Toast vertical="top" horizontal="end">
+        <Toast vertical="top" horizontal="center" className="w-96">
           <Alert status={toast.status}>
             <div className="w-full flex-row justify-between gap-2">
               <h3>{toast.text}</h3>
--- src/Layout/Pages/Questions/Questions.js
+++ src/Layout/Pages/Questions/Questions.js
@@ -1,20 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Card } from "react-daisyui";
 import { useLoaderData } from "react-router-dom";
 import Question from "../../../Components/Question/Question";
 
 const Questions = () => {
+  const [scores, setScores] = useState([]);
   const data = useLoaderData();
-  console.log(data);
+  const { logo, total, name } = data.data;
+
   const questions = data.data.questions;
   console.log(questions);
+  const verifyAnswer = (selectedAnswer, correctAnswer, setToast, id) => {
+    if (selectedAnswer === correctAnswer) {
+      setToast({ text: "Good Job!! Correct Answer.", status: "success" });
+      if (!scores.includes(id)) {
+        setScores([...scores, id]);
+      }
+    } else {
+      setToast({ text: "Sorry!!  Wrong Answer", status: "error" });
+    }
+    setTimeout(() => {
+      setToast("");
+    }, 3000);
+  };
+  console.log(scores);
   return (
-    <div className="container mx-auto">
-      <h1 className="text-2xl text-center mb-10">
-        Answer the following Questions by filling in the circles:
-      </h1>
-      {questions.map((q, index) => (
-        <Question questionData={q} serial={index + 1}></Question>
-      ))}
+    <div className="container mx-auto flex flex-col xl:flex-row gap-12">
+      <div>
+        <h1 className="text-2xl text-center mb-10">
+          Answer the following Questions by filling in the circles:
+        </h1>
+        {questions.map((q, index) => (
+          <Question
+            questionData={q}
+            serial={index + 1}
+            verifyAnswer={verifyAnswer}
+          ></Question>
+        ))}
+      </div>
+      <Card className="h-3/6 bg-slate-400 text-base-100">
+        <h1 className="text-center mt-2 text-3xl font-semibold">{name}</h1>
+        <Card.Image src={logo} alt="Shoes" />
+        <Card.Body className="items-center text-center">
+          <Card.Title tag="h2"> Score</Card.Title>
+          <p className="text-2xl font-semibold">
+            {scores.length !== 0 && `${scores.length}/${total}`}
+          </p>
+          <Card.Actions className="justify-end">
+            <Button>Quiz Complete</Button>
+          </Card.Actions>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
